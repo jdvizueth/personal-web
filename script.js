f67393b6ff5f18fdfab849692d1aa60c16162a0d
josev <jdv72@cornell.edu>
2023-06-02 17:55:50 -0400

    Implement project slideshow

--- script.js
+++ script.js
@@ -40,4 +40,45 @@ document.addEventListener('DOMContentLoaded', () => {
   typeLogo();
 });
 
+let currentIndex = 0;
+document.addEventListener('DOMContentLoaded', function () {
+  const projectItems = document.querySelectorAll('.project-item');
+  projectItems.forEach((item) => {
+    let prevArrow = item.querySelector('.prev-arrow');
+    let nextArrow = item.querySelector('.next-arrow');
+    const projectCount = projectItems.length - 1;
 
+    prevArrow.addEventListener('click', () => {
+      if (currentIndex - 1 < 0) {
+        currentIndex = projectCount
+      }
+      else {
+        currentIndex = currentIndex - 1
+      }
+      console.log('prevPasses')
+      updateProjectVisibility();
+    });
+
+    nextArrow.addEventListener('click', () => {
+      if (currentIndex + 1 > projectCount) {
+        currentIndex = 0
+      }
+      else {
+        currentIndex = currentIndex + 1
+      }
+      console.log('nextPasses')
+      updateProjectVisibility();
+    });
+
+    function updateProjectVisibility() {
+      projectItems.forEach((item, index) => {
+        item.style.display = index === currentIndex ? 'flex' : 'none';
+        console.log('current Index:', currentIndex)
+      });
+    }
+
+    // Initially show only the first project
+    updateProjectVisibility();
+    console.log('passes here')
+  });
+});
